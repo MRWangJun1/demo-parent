7e7f71f570a73ff8992988740a70ca6d6b1fe3ee
wangjun <wangjun@111.com.cn>
2022-03-24 16:07:45 +0800

    kafka

---
+++ demo-web/src/main/java/com/example/demo/web/controller/kafka/KafkaConsumer.java
@@ -0,0 +1,38 @@
+package com.example.demo.web.controller.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : wangjun
+ * @date : 2022/3/24  13:40
+ */
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = {"topic1"})
+    public void onMessage1(ConsumerRecord<?, ?> record) {
+        // 消费的哪个topic、partition的消息,打印出消息内容
+        System.out.println("简单消费1：" + record.topic() + "-" + record.partition() + "-" + record.value());
+    }
+
+
+    /**
+     * @Title 指定topic、partition、offset消费
+     * @Description 同时监听topic1和topic2，监听topic1的0号分区、topic2的 "0号和1号" 分区，指向1号分区的offset初始值为8
+     * @Author long.yuan
+     * @Date 2020/3/22 13:38
+     * @Param [record]
+     * @return void
+     **/
+    @KafkaListener(id = "consumer1",groupId = "felix-group",topicPartitions = {
+            @TopicPartition(topic = "topic1", partitions = { "0" }),
+            @TopicPartition(topic = "topic2", partitions = "0", partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "8"))
+    })
+    public void onMessage2(ConsumerRecord<?, ?> record) {
+        System.out.println("简单消费2:topic:"+record.topic()+"|partition:"+record.partition()+"|offset:"+record.offset()+"|value:"+record.value());
+    }
+}
