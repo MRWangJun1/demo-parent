1687af5f9ed206d459f8852c031de660ab71d604
wangjun <wangjun@111.com.cn>
2022-04-01 17:01:09 +0800

    提交~

---
+++ demo-service/src/main/java/com/example/demo/service/proxy/UserManager.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.proxy;
+
+/**
+ * @author : wangjun
+ * @date : 2022/3/30  10:07
+ */
+public interface UserManager {
+    public void addUser(String id, String password);
+    public void delUser(String id);
+}
